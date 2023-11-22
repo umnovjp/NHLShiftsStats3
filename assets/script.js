8e8e3c4143a3f8f4570a797a6e8f925b817d5f0f
umnovjp <umnovjp@yahoo.com>
2023-11-21 23:52:20 -0600

    just started shifts loop

--- assets/script.js
+++ assets/script.js
@@ -57,26 +57,39 @@ function getInputValue() {
                     standingsArray.push(data.homeTeam.abbrev, data_standings.standings[i].wins, data_standings.standings[i].losses, data_standings.standings[i].otLosses)
                     console.log(i, data.homeTeam.abbrev, data_standings.standings[i].wins, data_standings.standings[i].losses, data_standings.standings[i].otLosses)
                   }
-                  // else (console.log('No Such Abbrev'))
+                  // else (console.log('No Such Abbrev'));
                 }
             var gameTitle = document.createElement('h2'); gameTitle.textContent = '';
             gameTitle.innerHTML = 'You are watching stats for ' + data.awayTeam.abbrev + standingsArray[0] + ' W ' + standingsArray[1] + ' L ' + standingsArray[2] + ' O at ' + data.homeTeam.abbrev + standingsArray[3] + ' W ' + standingsArray[4] + ' L ' + standingsArray[5] + ' O game';
             document.getElementById('gameInfo').appendChild(gameTitle);
             const homeF = []; const awayF = []; const homeD = []; const awayD = []; const homeG = []; const awayG = []; 
             console.log(data.boxscore.playerByGameStats.awayTeam.forwards, data.boxscore.playerByGameStats.awayTeam.defense, data.boxscore.playerByGameStats.awayTeam.goalies, data.boxscore.playerByGameStats.homeTeam.forwards, data.boxscore.playerByGameStats.awayTeam.defense, data.boxscore.playerByGameStats.awayTeam.goalies);
-            var obj = data.boxscore.playerByGameStats.homeTeam.forwards; var keys = Object.keys(obj); 
+            var obj = data.boxscore.playerByGameStats.homeTeam.forwards; var keys = Object.keys(obj);
             for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; homeF.push(val.playerId, val.sweaterNumber, val.name.default) }
-            var obj = data.boxscore.playerByGameStats.homeTeam.defense; var keys = Object.keys(obj); 
+            var obj = data.boxscore.playerByGameStats.homeTeam.defense; var keys = Object.keys(obj);
             for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; homeD.push(val.playerId, val.sweaterNumber, val.name.default) }
-            var obj = data.boxscore.playerByGameStats.homeTeam.goalies; var keys = Object.keys(obj); 
+            var obj = data.boxscore.playerByGameStats.homeTeam.goalies; var keys = Object.keys(obj);
             for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; homeG.push(val.playerId, val.sweaterNumber, val.name.default) }
-            var obj = data.boxscore.playerByGameStats.awayTeam.forwards; var keys = Object.keys(obj); 
+            var obj = data.boxscore.playerByGameStats.awayTeam.forwards; var keys = Object.keys(obj);
             for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; awayF.push(val.playerId, val.sweaterNumber, val.name.default) }
-            var obj = data.boxscore.playerByGameStats.awayTeam.defense; var keys = Object.keys(obj); 
+            var obj = data.boxscore.playerByGameStats.awayTeam.defense; var keys = Object.keys(obj);
             for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; awayD.push(val.playerId, val.sweaterNumber, val.name.default) }
-            var obj = data.boxscore.playerByGameStats.awayTeam.goalies; var keys = Object.keys(obj); 
+            var obj = data.boxscore.playerByGameStats.awayTeam.goalies; var keys = Object.keys(obj);
             for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; awayG.push(val.playerId, val.sweaterNumber, val.name.default) }
             console.log(homeF, homeD, homeG, awayF, awayD, awayF)
+
+            var shiftsURL = 'https://cors-anywhere.herokuapp.com/https://api.nhle.com/stats/rest/en/shiftcharts?cayenneExp=gameId=' + gameId;
+            fetch(shiftsURL, {
+              "method": "GET", "headers": {}
+            })
+              .then(function (response) {
+                return response.json();
+              })
+              .then(function (data_shifts) {
+                console.log(data_shifts);
+               
+          }); // end second .then shifts
+
           }); // end second .then standings
           }); // end second .then gamecenter
       } // end displayGameData         

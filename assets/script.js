31a5c62ffe4bbd33b4d1016ac1f56e12c25e3f40
umnovjp <umnovjp@yahoo.com>
2023-12-08 23:42:52 -0600

    started writing 5x5 loop but it is not going fast

--- assets/script.js
+++ assets/script.js
@@ -134,36 +134,24 @@ function getInputValue() {
                     console.log(Object.keys(playerIdeObject), Object.values(playerIdeObject));
                     console.log(dArray); pairingsArray = [[], [], [], [], [], []];
                     for (h = 0; h < 2; h++) { // h = 0 home team D, h = 1 away team D
-                      for (i = 0; i < 3; i++) {
-                        for (j = 0; j < dArray[h].length; j++) {
-                          for (k = j + 1; k < dArray[h].length; k++) {
-                            tempTime = [];
-                            for (l = 0; l < dArray[h][j][i].length / 2; l++) {
-                              for (m = 0; m < dArray[h][k][i].length / 2; m++) {
-                                if ((dArray[h][k][i][2 * m] >= dArray[h][j][i][2 * l]) && (dArray[h][k][i][2 * m] <= dArray[h][j][i][2 * l + 1])) {
+                      for (i = 0; i < 3; i++) { for (j = 0; j < dArray[h].length; j++) {
+                          for (k = j + 1; k < dArray[h].length; k++) {tempTime = []; for (l = 0; l < dArray[h][j][i].length / 2; l++) {
+                              for (m = 0; m < dArray[h][k][i].length / 2; m++) { if ((dArray[h][k][i][2 * m] >= dArray[h][j][i][2 * l]) && (dArray[h][k][i][2 * m] <= dArray[h][j][i][2 * l + 1])) {
                                   if (dArray[h][k][i][2 * m + 1] >= dArray[h][j][i][2 * l + 1]) { tempTime.push(dArray[h][j][i][2 * l + 1] - dArray[h][k][i][2 * m]) }
-                                  else { tempTime.push(dArray[h][k][i][2 * m + 1] - dArray[h][k][i][2 * m]) }
-                                }
+                                  else { tempTime.push(dArray[h][k][i][2 * m + 1] - dArray[h][k][i][2 * m]) }}
                                 else if ((dArray[h][k][i][2 * m] <= dArray[h][j][i][2 * l]) && (dArray[h][k][i][2 * m + 1] >= dArray[h][j][i][2 * l])) {
                                   if (dArray[h][k][i][2 * m + 1] >= dArray[h][j][i][2 * l + 1]) { tempTime.push(dArray[h][j][i][2 * l + 1] - dArray[h][j][i][2 * l]) }
                                   else { tempTime.push(dArray[h][k][i][2 * m + 1] - dArray[h][j][i][2 * l]) }
-                                }
-                              }
-                            }   // end l loop
+                                }}}   // end l loop
                             shifts = 0; const sum = tempTime.reduce((partialSum, a) => partialSum + a, 0);
                             for (n = 0; n < tempTime.length; n++) { if (tempTime[n] >= 10) { shifts = shifts + 1 } }
                             pairingsArray[i + 3 * h].push(sum); pairingsArray[i + 3 * h].push(shifts); // console.log(i, j, k, tempTime);
-                          }
-                        }
-                      }
-                    } // end i and h loop periods
-                    console.log(pairingsArray); 
-                    for (i = 0; i < 2; i++) { dArrayTemp = dArray[i][0][0] // home and away teams
-                      for (j = 0; j < dArray[i].length; j++) {
-                        for (k = j + 1; k < dArray[i].length; k++) { dArrayTemp = dArrayTemp.concat(dArray[i][k][0]);
-                        console.log(dArrayTemp) }
-                      }
-                    }
+                          }}}} // end i and h loop periods
+                    console.log(pairingsArray); dArrayTemp = [[[],[],[]],[[],[],[]]];
+                    for (i = 0; i < 2; i++) { for (j = 0; j < dArray[i].length; j++) { for (k = 0; k < 3; k++) {dArrayTemp[i][k] = dArrayTemp[i][k].concat(dArray[i][j][k])}}}
+                    console.log(dArrayTemp);
+                    dArrayTempSplit = [[[],[]],[]]
+
                     // adding home team defense to screen 
                     firstDNumber.innerHTML = homeD[1] + ' ' + homeD[2]; secondDNumber.innerHTML = homeD[4] + ' ' + homeD[5]; thirdDNumber.innerHTML = homeD[7] + ' ' + homeD[8];
                     firstD1.innerHTML = 'X' + '<br>' + 'X' + '<br>' + 'X'; secondD2.innerHTML = 'X' + '<br>' + 'X' + '<br>' + 'X'; thirdD3.innerHTML = 'X' + '<br>' + 'X' + '<br>' + 'X'; forthD4.innerHTML = 'X' + '<br>' + 'X' + '<br>' + 'X';

5eadd4d62945dfb8ea0ecf6d4c50688e0106f460
umnovjp <umnovjp@yahoo.com>
2023-12-20 23:29:01 -0600

    not a major update but made sure 5x5 cycle works for now

--- assets/script.js
+++ assets/script.js
@@ -132,29 +132,30 @@ function getInputValue() {
                     for (i = 0; i < 2; i++) { for (j = 0; j < fArray[i].length; j++) { for (k = 0; k < 3; k++) {fArrayTemp[i][k] = fArrayTemp[i][k].concat(fArray[i][j][k])}}}
                     console.log(dArrayTemp, fArrayTemp);
                     // this one is not currently used it is created for 5x5 loop
-                    dArrayTempSplit = [[[],[],[]],[[],[],[]]]; dArrayTempSplit2 = [[[],[],[]],[[],[],[]]]; dArrayTempSplit3 = [[[],[],[]],[[],[],[]]];// dArrayTempSplit2Ordered = [[[],[],[]],[[],[],[]]]; 
-                    for (i = 0; i < 2; i++) {for (j = 0; j < 3; j++) {for (k = 0; k < dArrayTemp[i][j].length/2; k++){dArrayTempSplit[i][j].push(dArrayTemp[i][j][2*k])
-                    dArrayTempSplit2[i][j].push(dArrayTemp[i][j][2*k]);
-                    dArrayTempSplit2[i][j] = dArrayTempSplit2[i][j].sort((a, b) => a - b)}
-                    for (k = 0; k < dArrayTempSplit2[i][j].length; k++) { //console.log(dArrayTempSplit[i][j].indexOf(dArrayTempSplit2[i][j][k]), dArrayTempSplit[i][j].lastIndexOf(dArrayTempSplit2[i][j][k]))
-                      if (dArrayTempSplit3[i][j].includes(dArrayTempSplit[i][j].indexOf(dArrayTempSplit[i][j][2*k]))){}
-                  else dArrayTempSplit3[i][j].push(k)}
-                  }}
-                    console.log(dArrayTempSplit, dArrayTempSplit2, dArrayTempSplit3);
+                  //   dArrayTempSplit = [[[],[],[]],[[],[],[]]]; dArrayTempSplit2 = [[[],[],[]],[[],[],[]]]; dArrayTempSplit3 = [[[],[],[]],[[],[],[]]];// dArrayTempSplit2Ordered = [[[],[],[]],[[],[],[]]]; 
+                  //   for (i = 0; i < 2; i++) {for (j = 0; j < 3; j++) {for (k = 0; k < dArrayTemp[i][j].length/2; k++){dArrayTempSplit[i][j].push(dArrayTemp[i][j][2*k])
+                  //   dArrayTempSplit2[i][j].push(dArrayTemp[i][j][2*k]);
+                  //   dArrayTempSplit2[i][j] = dArrayTempSplit2[i][j].sort((a, b) => a - b)}
+                  //   for (k = 0; k < dArrayTempSplit2[i][j].length; k++) { //console.log(dArrayTempSplit[i][j].indexOf(dArrayTempSplit2[i][j][k]), dArrayTempSplit[i][j].lastIndexOf(dArrayTempSplit2[i][j][k]))
+                  //     if (dArrayTempSplit3[i][j].includes(dArrayTempSplit[i][j].indexOf(dArrayTempSplit[i][j][2*k]))){}
+                  // else dArrayTempSplit3[i][j].push(k)}
+                  // }}
+                  //   console.log(dArrayTempSplit, dArrayTempSplit2, dArrayTempSplit3);
 
                     // new attempt to create 5x5 loop
-                    dArrayTemp2 = [[[],[],[]],[[],[],[]]]; dArrayTemp3 = [[[],[],[]],[[],[],[]]];
-                    for (i = 0; i < 2; i++) {for (j = 0; j < 3; j++) {for (k = 0; k < 1200; k++) {dArrayTemp2[i][j].push(0)}}}
+                    dArrayTemp2 = [[[],[],[]],[[],[],[]]]; dArrayTemp3 = [[[],[],[]],[[],[],[]]]; fArrayTemp2 = [[[],[],[]],[[],[],[]]]; 
+                    for (i = 0; i < 2; i++) {for (j = 0; j < 3; j++) {for (k = 0; k < 1200; k++) {dArrayTemp2[i][j].push(0); fArrayTemp2[i][j].push(0)}}}
                     for (i = 0; i < 2; i++) {for (j = 0; j < 3; j++) for (k = 0; k < dArrayTemp[i][j].length/2; k++) 
                   {for (l = dArrayTemp[i][j][2*k]; l < dArrayTemp[i][j][2*k + 1]; l++) {dArrayTemp2[i][j][l] = dArrayTemp2[i][j][l] + 1}}}
+                  for (i = 0; i < 2; i++) {for (j = 0; j < 3; j++) for (k = 0; k < fArrayTemp[i][j].length/2; k++) 
+                  {for (l = fArrayTemp[i][j][2*k]; l < fArrayTemp[i][j][2*k + 1]; l++) {dArrayTemp2[i][j][l] = dArrayTemp2[i][j][l] + 1}}}
                   
                   for (i = 0; i < 2; i++) {for (j = 0; j < 3; j++) {for (k = 0; k < 1200; k++) {if (dArrayTemp2[i][j][k+1] === dArrayTemp2[i][j][k]) {delete dArrayTemp2[i][j][k]}
                 }}} //  {dArrayTemp2[i][j].shift()}
                 
                 for (i = 0; i < 2; i++) {for (j = 0; j < 3; j++) {for (k = 0; k < 1200; k++) {if (!dArrayTemp2[i][j][k]) {} else {dArrayTemp3[i][j].push(dArrayTemp2[i][j][k], k)}
               }}}
-                  console.log(dArrayTemp3)
-                  // 41 3, 
+                  console.log(dArrayTemp2, dArrayTemp3)
 
                   fiveOnFive = [[[0, 601, 700, 1200], [0,601, 700, 1200], [0, 601, 700, 1200]],[[0, 601, 700, 1200], [0, 601, 700, 1200], [0,601, 700, 1200]]];
                     for (h = 0; h < 2; h++) { // h = 0 home team D, h = 1 away team D

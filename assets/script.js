64b8d958caba2a0208e5da4976c3a0ccbfd2928f
umnovjp <umnovjp@yahoo.com>
2024-01-10 23:40:19 -0600

    completed away team cycle it works 97 percent of the games

--- assets/script.js
+++ assets/script.js
@@ -343,7 +343,7 @@ function getInputValue() {
                    for (i = 0; i < linesArray2[4].length; i++) {if ((linesArray2[4][i]===linesArray4[tempIndex+2])||(linesArray2[4][i]===linesArray4[tempIndex+3])||(linesArray2[4][i]===linesArray4[tempIndex+4])) {}
                   else linesArray5.push(linesArray2[4][i])}
                   console.log(linesArray5, linesArray5.length)
-                  if (linesArray5.length===2){fourthLine.innerHTML = homeF[3*linesArray5[0]+1] + ' ' + homeF[3*linesArray5[0]+2] + '<br>' + homeF[3*linesArray5[1]+1] + ' ' + homeF[3*linesArray5[1]+2]}
+                  if (linesArray5.length===2){fourthLine.innerHTML = homeF[3*linesArray5[0]+1] + ' ' + homeF[3*linesArray5[1]+2] + '<br>' + homeF[3*linesArray5[1]+1] + ' ' + homeF[3*linesArray5[1]+2]}
                   else if (linesArray5.length===3){fourthLine.innerHTML = homeF[3*linesArray5[0]+1] + ' ' + homeF[3*linesArray5[0]+2] + '<br>' + homeF[3*linesArray5[1]+1] + ' ' + homeF[3*linesArray5[1]+2]  + '<br>' + homeF[3*linesArray5[2]+1] + ' ' + homeF[3*linesArray5[2]+2];
                   for (i = 0; i < linesArray[0].length/5; i++) {if ((linesArray[0][5*i+2] === linesArray5[0])&&(linesArray[0][5*i+3] === linesArray5[1])&&(linesArray[0][5*i+4] === linesArray5[2]))
                     {// linesArray4.push(linesArray2[0][5*i], linesArray2[0][5*i+1], linesArray2[1][5*i], linesArray2[1][5*i+1], linesArray2[2][5*i], linesArray2[2][5*i+1]);
@@ -363,13 +363,25 @@ function getInputValue() {
                     fourthLineTimeAway.innerHTML = linesArray4[0] + '<br>' + linesArray4[2] + '<br>' + linesArray4[4]}}
                     if(linesArray2[3].length<=7) {linesArray4 = []; for(i=3; i<6; i++){for (j = 0; j<linesArray[i].length/5; j++){for (k=0; k<linesArray2[5].length-2; k++) {for (l=k+1; l<linesArray2[5].length-1; l++){for (m=l+1; m<linesArray2[5].length; m++){
                       if((linesArray2[5][k] === linesArray[i][5*j+2])&&(linesArray2[5][l] === linesArray[i][5*j+3])&&(linesArray2[5][m] === linesArray[i][5*j+4])){linesArray4.push(linesArray[i][5*j], linesArray[i][5*j+1], linesArray[i][5*j+2], linesArray[i][5*j+3], linesArray[i][5*j+4])}                      
-                    }}}}}}
+                    }}}}}
                     tempIndex = linesArray4.indexOf(Math.max(...linesArray4)); tempIndex2 = tempIndex%(linesArray4.length/3);
                     console.log(linesArray4, Math.max(...linesArray4), tempIndex, tempIndex2);
-                    thirdLineAway.innerHTML = homeF[3*linesArray4[tempIndex+2]+1] + ' ' + homeF[3*linesArray4[tempIndex+2]+2] + '<br>' + homeF[3*linesArray4[tempIndex+3]+1] + ' ' + homeF[3*linesArray4[tempIndex+3]+2]+ '<br>' + homeF[3*linesArray4[tempIndex+4]+1] + ' ' + homeF[3*linesArray4[tempIndex+4]+2];
+                    thirdLineAway.innerHTML = awayF[3*linesArray4[tempIndex+2]+1] + ' ' + awayF[3*linesArray4[tempIndex+2]+2] + '<br>' + awayF[3*linesArray4[tempIndex+3]+1] + ' ' + awayF[3*linesArray4[tempIndex+3]+2]+ '<br>' + awayF[3*linesArray4[tempIndex+4]+1] + ' ' + awayF[3*linesArray4[tempIndex+4]+2];
                     thirdLineTimeAway.innerHTML = linesArray4[tempIndex2] + '<br>' + linesArray4[tempIndex2 + linesArray4.length/3] + '<br>' + linesArray4[tempIndex2 + 2*linesArray4.length/3]
                     thirdLineTime2Away.innerHTML = linesArray4[tempIndex2+1] + '<br>' + linesArray4[tempIndex2 + 1 + linesArray4.length/3] + '<br>' + linesArray4[tempIndex2 + 2 + 2*linesArray4.length/3];
                     console.log(linesArray4[tempIndex+2], linesArray4[tempIndex+3], linesArray4[tempIndex+4]);
+                    linesArray5 = [];
+                    for (i = 0; i < linesArray2[5].length; i++) {if ((linesArray2[5][i]===linesArray4[tempIndex+2])||(linesArray2[5][i]===linesArray4[tempIndex+3])||(linesArray2[5][i]===linesArray4[tempIndex+4])) {}
+                    else linesArray5.push(linesArray2[5][i])}
+                    console.log(linesArray5, linesArray5.length);
+                    if (linesArray5.length===2){fourthLineAway.innerHTML = awayF[3*linesArray5[0]+1] + ' ' + awayF[3*linesArray5[0]+2] + '<br>' + awayF[3*linesArray5[1]+1] + ' ' + awayF[3*linesArray5[1]+2]}
+                    else if (linesArray5.length===3){fourthLineAway.innerHTML = awayF[3*linesArray5[0]+1] + ' ' + awayF[3*linesArray5[0]+2] + '<br>' + awayF[3*linesArray5[1]+1] + ' ' + awayF[3*linesArray5[1]+2]  + '<br>' + awayF[3*linesArray5[2]+1] + ' ' + awayF[3*linesArray5[2]+2];
+                    for (i = 0; i < linesArray[3].length/5; i++) {if ((linesArray[3][5*i+2] === linesArray5[0])&&(linesArray[3][5*i+3] === linesArray5[1])&&(linesArray[3][5*i+4] === linesArray5[2]))
+                    {console.log(i); tempIndex = i}}
+                    fourthLineTimeAway.innerHTML = linesArray[3][5*tempIndex] + '<br>' + linesArray[4][5*tempIndex] + '<br>' + linesArray[5][5*tempIndex];
+                    fourthLineTime2Away.innerHTML = linesArray[3][5*tempIndex+1] + '<br>' + linesArray[4][5*tempIndex+1] + '<br>' + linesArray[5][5*tempIndex+1]}
+                    else if (linesArray5.length===4) {console.log('will be added when required')}
+                    }
                     // })
                     console.log(linesArray2); linesArray3 = [[],[]];
                     for (i=2; i<4; i++) if (linesArray2[i]>12) {

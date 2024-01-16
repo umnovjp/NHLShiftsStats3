87d17ddc3979852783cc6e98e4440d04bfa15e55
umnovjp <umnovjp@yahoo.com>
2024-01-15 23:17:34 -0600

    major progress if only one away F line is determined but have to detremine 4th line yet

--- assets/script.js
+++ assets/script.js
@@ -402,23 +402,28 @@ function getInputValue() {
                     console.log(linesArray4, Math.max(...linesArray4), tempIndex, tempIndex2); 
                     secondLineAway.innerHTML = awayF[3*linesArray4[tempIndex+2]+1] + ' ' + awayF[3*linesArray4[tempIndex+2]+2] + '<br>' + awayF[3*linesArray4[tempIndex+3]+1] + ' ' + awayF[3*linesArray4[tempIndex+3]+2]+ '<br>' + awayF[3*linesArray4[tempIndex+4]+1] + ' ' + awayF[3*linesArray4[tempIndex+4]+2];
                     secondLineTimeAway.innerHTML = linesArray4[tempIndex2] + '<br>' + linesArray4[tempIndex2 + linesArray4.length/3] + '<br>' + linesArray4[tempIndex2 + 2*linesArray4.length/3]
-                    secondLineTime2Away.innerHTML = linesArray4[tempIndex2+1] + '<br>' + linesArray4[tempIndex2 + 1 + linesArray4.length/3] + '<br>' + linesArray4[tempIndex2 + 2 + 2*linesArray4.length/3];
+                    secondLineTime2Away.innerHTML = linesArray4[tempIndex2+1] + '<br>' + linesArray4[tempIndex2 + 1 + linesArray4.length/3] + '<br>' + linesArray4[tempIndex2 + 1 + 2*linesArray4.length/3];
                     console.log(linesArray4[tempIndex+2], linesArray4[tempIndex+3], linesArray4[tempIndex+4]);
                     linesArray5 = [];
                     for (i = 0; i < awayF.length/3; i++) {if ((i === linesArray2[3][0])||(i === linesArray2[3][1])||(i === linesArray2[3][2])||(i === linesArray4[tempIndex + 2])||(i === linesArray4[tempIndex + 3])||(i === linesArray4[tempIndex + 4])) {}
-                  else {linesArray5.push(i)}}                   
-                    console.log(linesArray5);
-                    linesArray6 = []; // linesArray7 = [];
-                  for (i = 3; i < 6; i++) {for (j = 0; j<linesArray[i].length/5; j++) {for (k=0; k<linesArray5.length-2; k++) {for (l=k+1; l<linesArray5.length-1; l++) {for (m=l+1; m<linesArray5.lenth; m++){
-                    console.log(i,k,l,m)
-                    if((linesArray5[k]===linesArray[i][5*j+2])&&(linesArray5[l]===linesArray[i][5*j+3])&&(linesArray5[m]===linesArray[i][5*j+4])) { console.log('second cycle')
-                      linesArray6.push(linesArray[i][5*j], linesArray[i][5*j+1], linesArray[i][5*j+2]. linesArray[i][5*j+3]. linesArray[i][5*j+4]);                      
-                      tempIndex = linesArray6.indexOf(Math.max(...linesArray6)); tempIndex2 = tempIndex%(linesArray6.length/3); 
-                      console.log(linesArray6,  Math.max(...linesArray6), tempIndex, tempIndex2);
-                    }
+                  else {linesArray5.push(i)}}
+                    console.log(linesArray5); linesArray6 = [];
+                  for (i = 3; i < 6; i++) {
+                    for (j = 0; j<linesArray[i].length/5; j++) {for (k=0; k<linesArray5.length-2; k++) {for (l=k+1; l<linesArray5.length-1; l++) {for (m=l+1; m<linesArray5.length; m++) 
+                      {if((linesArray5[k]===linesArray[i][5*j+2])&&(linesArray5[l]===linesArray[i][5*j+3])&&(linesArray5[m]===linesArray[i][5*j+4])) { console.log('second cycle')
+                      linesArray6.push(linesArray[i][5*j], linesArray[i][5*j+1], linesArray[i][5*j+2], linesArray[i][5*j+3], linesArray[i][5*j+4]);                      
+                      tempIndex = linesArray6.indexOf(Math.max(...linesArray6)); tempIndex2 = tempIndex%(linesArray6.length/3)}
                   }}}}}
+                console.log(linesArray6,  Math.max(...linesArray6), tempIndex, tempIndex2);
+                thirdLineAway.innerHTML = awayF[3*linesArray6[tempIndex+2]+1] + ' ' + awayF[3*linesArray6[tempIndex+2]+2] + '<br>' + awayF[3*linesArray6[tempIndex+3]+1] + ' ' + awayF[3*linesArray6[tempIndex+3]+2]+ '<br>' + awayF[3*linesArray6[tempIndex+4]+1] + ' ' + awayF[3*linesArray6[tempIndex+4]+2];
+                    thirdLineTimeAway.innerHTML = linesArray6[tempIndex2] + '<br>' + linesArray6[tempIndex2 + linesArray6.length/3] + '<br>' + linesArray6[tempIndex2 + 2*linesArray6.length/3];
+                    thirdLineTime2Away.innerHTML = linesArray6[tempIndex2+1] + '<br>' + linesArray6[tempIndex2 + 1 + linesArray6.length/3] + '<br>' + linesArray6[tempIndex2 + 1 + 2*linesArray6.length/3];
+                    linesArray7 = [];
+                    console.log(linesArray6[tempIndex+2], linesArray6[tempIndex+3], linesArray6[tempIndex+4])
+                    for (i = 0; i < linesArray5.length; i++) {// to continue here tomorrow
+                    }
                   }
-                    // })
+                    
                     console.log(linesArray2); linesArray3 = [[],[]];
                     // to check if any players repeats in the lines H and A that will work if there are 5 lines
                     for (i=2; i<4; i++) {if (linesArray2[i].length>12) {

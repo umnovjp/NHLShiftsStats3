9a87157ba4a8843b239f28f7a528b51417688788
umnovjp <umnovjp@yahoo.com>
2024-01-24 23:54:41 -0600

    5F lines loop finally works for home team great progress

--- assets/script.js
+++ assets/script.js
@@ -431,17 +431,17 @@ function getInputValue() {
                     // to check if any players repeats in the lines H and A that will work if there are 5 lines 
                     for (i=2; i<4; i++) {if (linesArray2[i].length>12) {for (j = 0; j < linesArray2[i].length; j++) {for (k = j + 1; k < linesArray2[i].length; k++) {if (linesArray2[i][j] === linesArray2[i][k]){linesArray3[i-2].push(linesArray2[i][j])}}
                     }
-                  if (linesArray3[i-2].length>0) {for (j = 0; j < linesArray3[i-2].length; j++) {console.log('test case', linesArray3[i-2][j]);
-                  for (k = 0; k<linesArray2[i].length/3; k++) {if ((linesArray3[i-2].includes(linesArray2[i][3*k]))||(linesArray3[i-2].includes(linesArray2[i][3*k+1]))||(linesArray3[i-2].includes(linesArray2[i][3*k+2]))) { }
-                  else { if (linesArray8[i-2].includes(linesArray2[i][3*k])) {}
+                  if (linesArray3[i-2].length>0) {for (j = 0; j < linesArray3[i-2].length; j++) {//console.log('test case', linesArray3[i-2][j]);
+                  for (k=0; k<linesArray2[i].length/3; k++) {if ((linesArray3[i-2].includes(linesArray2[i][3*k]))||(linesArray3[i-2].includes(linesArray2[i][3*k+1]))||(linesArray3[i-2].includes(linesArray2[i][3*k+2]))) { }
+                  else {if (linesArray8[i-2].includes(linesArray2[i][3*k])) {}
                   else {linesArray8[i-2].push(linesArray2[i][3*k], linesArray2[i][3*k+1], linesArray2[i][3*k+2])}}
                 }}
               for (j=0; j<linesArray2[i].length/3; j++) {tempIndex = 0; for (k=0; k<3; k++) {
-              if (linesArray3[i-2].includes(linesArray2[i][3*j+k]) ) {tempIndex=tempIndex+1; console.log(tempIndex)}}
-            if (tempIndex===1) {console.log(linesArray2[i][3*j], linesArray2[i][3*j+1], linesArray2[i][3*j+2]);
-              linesArray8[i-2].push(linesArray2[i][3*j], linesArray2[i][3*j+1], linesArray2[i][3*j+2])}
-              // new script will be added here 
-          }              
+              if (linesArray3[i-2].includes(linesArray2[i][3*j+k])) {tempIndex=tempIndex+1; console.log(tempIndex)}}
+            if (tempIndex===1) {// console.log(linesArray2[i][3*j], linesArray2[i][3*j+1], linesArray2[i][3*j+2]);
+              linesArray8[i-2].push(linesArray2[i][3*j], linesArray2[i][3*j+1], linesArray2[i][3*j+2])
+            if (linesArray8[i-2].length=9) {for (k=0; k<homeF.length/3; k++) {if (linesArray8[i-2].includes(k)) {}
+          else (linesArray8[i-2].push(k))}}}}
             }}} // end i loop for 2 teams and if loop if there are > 4 lines 
                     console.log(linesArray3, linesArray8); // end check repeating F player
 

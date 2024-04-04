080222f913e026649899145da012fd23cdf8277c
umnovjp <umnovjp@yahoo.com>
2024-04-03 23:34:31 -0500

    another minor update becaus eof lack of time

--- assets/test_script.jsx
+++ assets/test_script.jsx
@@ -6,7 +6,7 @@ for (h = 0; h < 2; h++) {for (i = 0; i < 3; i++) { for (j = 0; j < fArray[h].len
             else if ((fArray[h][k][i][2 * m] <= fArray[h][j][i][2 * l]) && (fArray[h][k][i][2 * m + 1] >= fArray[h][j][i][2 * l])) {
               if (fArray[h][k][i][2 * m + 1] >= fArray[h][j][i][2 * l + 1]) { shiftsPair.push(fArray[h][j][i][2 * l], fArray[h][j][i][2 * l + 1]) }
               else {shiftsPair.push(fArray[h][j][i][2 * l], fArray[h][k][i][2 * m + 1])}
-            }}}// end m, l loop
+            }}}// end m, l loop 
             for (l = k + 1; l < fArray[h].length; l++) {tempTime = []; tempTime2 = [];// if (i === 0  && h === 0) { console.log(j, k, l, shiftsPair);}
             for (m = 0; m < shiftsPair.length/2; m++){
               for (n = 0; n < fArray[h][l][i].length/2; n++) {if ((fArray[h][l][i][2*n]>=shiftsPair[2*m])&&(fArray[h][l][i][2*n]<shiftsPair[2*m+1])){

f07609eefadd01c32804211ef8ad7d43ed7c24a2
umnovjp <umnovjp@yahoo.com>
2024-05-08 18:20:15 -0500

    not a major update agaun because of travel

--- assets/script.js
+++ assets/script.js
@@ -429,8 +429,10 @@ function getInputValue() {
 
                     allLinesCombined = [[],[],[],[],[],[]];
                     for (h=0;h<2;h++) {for (i=0;i<3;i++) {for (j=0;j<linesArray[3*h+i].length/5;j++) {if ((linesArray[3*h+i][5*j]>75)&&(linesArray[3*h+i][5*j+1]>3)){allLinesCombined[3*h+i].push(linesArray[3*h+i][5*j+2],linesArray[3*h+i][5*j+3],linesArray[3*h+i][5*j+4])}}
-                    if (i>0) { for (j=0;j<finalLineup[h+2].length/3;j++) {for (k=0;k<allLinesCombined[3*h+i].length/3;k++) {if ((finalLineup[h+2][3*j]===allLinesCombined[3*h+i][3*k])&&(finalLineup[h+2][3*j+1]===allLinesCombined[3*h+i][3*k+1])&&(finalLineup[h+2][3*j+2]===allLinesCombined[3*h+i][3*k+2])){}
-                else (console.log(h, allLinesCombined[3*h+i][3*k], allLinesCombined[3*h+i][3*k+1], allLinesCombined[3*h+i][3*k+2], 'is the new line'))}}}
+                    if (i>0) { for (j=0;j<finalLineup[h+2].length/3;j++) {for (k=0;k<allLinesCombined[3*h+i].length/3;k++) {if (finalLineup[h+2][3*j]===allLinesCombined[3*h+i][3*k]) {
+                      if ((finalLineup[h+2][3*j+1]===allLinesCombined[3*h+i][3*k+1])&&(finalLineup[h+2][3*j+2]===allLinesCombined[3*h+i][3*k+2])) {}
+                      else {console.log(h, i, allLinesCombined[3*h+i][3*k], allLinesCombined[3*h+i][3*k+1], allLinesCombined[3*h+i][3*k+2],'is the new line', finalLineup[h+2][3*j], finalLineup[h+2][3*j+1], finalLineup[h+2][3*j+2], 'is the old line')}
+                    }}}}
               }}
                     console.log(allLinesCombined);
                     
